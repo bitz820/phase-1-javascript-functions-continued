28035bca92ed0f371d86f7d168d06b9b1a32e453
Mark <mark.n.bacon.jr@gmail.com>
2022-03-14 16:40:14 -0400

    commit tree 55bd3c9ffbac9e5675972bd6873e2b259d42c1cf

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (typeof activity !== 'undefined') {
+        return `This Saturday, I want to ${activity}!`
+    } else {
+        return `This Saturday, I want to roller-skate!`
+    }
+}
+
+function mondayWork(activity) {
+    return typeof activity !== "undefined" ? `This Monday, I will ${activity}.` : `This Monday, I will go to the office.`
+}
+
+function wrapAdjective(flair) {
+    return function innerFunc(adj) {
+        return `You are ${flair}${adj}${flair}!`
+    };
+    // return innerFunc(adj)
+}
+
+wrapAdjective(flair)(adj)
